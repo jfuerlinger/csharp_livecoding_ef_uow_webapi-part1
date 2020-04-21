8ee3b171c7cc93a4f95043ca54d353fdfe5642ba
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-04-21 13:01:12 +0200

    Cleanup template

--- source/MovieManager.Core/DataTransferObjects/MovieDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieManager.Core.DataTransferObjects
-{
-    public class MovieDto
-    {
-        public string Title { get; set; }
-        public int Year { get; set; }
-        public int Duration { get; set; }
-    }
-}
